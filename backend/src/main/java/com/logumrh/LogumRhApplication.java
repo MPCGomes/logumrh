7733ad7b64f4c5b132e9569db68d4cf5aff874b8
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-08 17:44:00 -0300

    add spring boot setup

---
+++ backend/src/main/java/com/logumrh/LogumRhApplication.java
@@ -0,0 +1,13 @@
+package com.logumrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogumRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogumRhApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/logumrh/LogumRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logumrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogumRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
