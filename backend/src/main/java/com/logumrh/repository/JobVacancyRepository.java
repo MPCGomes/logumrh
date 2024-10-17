0839305c1431ff0b570c5d83dd71351a939e0cd5
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-17 14:04:21 -0300

    update job vacancy repository to migration

--- backend/src/main/java/com/logumrh/repository/JobVacancyRepository.java
+++ backend/src/main/java/com/logumrh/repository/JobVacancyRepository.java
@@ -2,6 +2,11 @@ package com.logumrh.repository;
 
 import com.logumrh.model.JobVacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface JobVacancyRepository extends JpaRepository<JobVacancy, Integer> {
+import java.util.List;
+
+@Repository
+public interface JobVacancyRepository extends JpaRepository<JobVacancy, Long> {
+    List<JobVacancy> findByCompanyId(Long companyId);
 }
