b81284d4802de302d7a4895e5614aad850088217
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-17 14:03:28 -0300

    update job vacancy service to migration

--- backend/src/main/java/com/logumrh/service/JobVacancyService.java
+++ backend/src/main/java/com/logumrh/service/JobVacancyService.java
@@ -1,5 +1,6 @@
 package com.logumrh.service;
 
+import com.logumrh.dto.JobVacancyDTO;
 import com.logumrh.model.JobVacancy;
 import com.logumrh.repository.JobVacancyRepository;
 import org.springframework.stereotype.Service;
@@ -9,21 +10,27 @@ import java.util.List;
 @Service
 public class JobVacancyService {
 
-    public final JobVacancyRepository jobVacancyRepository;
+    private final JobVacancyRepository jobVacancyRepository;
 
     public JobVacancyService(JobVacancyRepository jobVacancyRepository) {
         this.jobVacancyRepository = jobVacancyRepository;
     }
 
-    public List<JobVacancy> getAllJobVacancies() {
-        return jobVacancyRepository.findAll();
-    }
-
-    public JobVacancy createJobVacancy(JobVacancy jobVacancy) {
+    public JobVacancy createJobVacancy(JobVacancyDTO jobVacancyDTO) {
+        JobVacancy jobVacancy = new JobVacancy();
+        jobVacancy.setTitle(jobVacancyDTO.getTitle());
+        jobVacancy.setCode(jobVacancyDTO.getCode());
+        jobVacancy.setModality(jobVacancyDTO.getModality());
+        jobVacancy.setDescription(jobVacancyDTO.getDescription());
+        jobVacancy.setSalary(jobVacancyDTO.getSalary());
         return jobVacancyRepository.save(jobVacancy);
     }
 
-    public void deleteJobVacancy(Integer id) {
+    public List<JobVacancy> findByCompanyId(Long companyId) {
+        return jobVacancyRepository.findByCompanyId(companyId);
+    }
+
+    public void deleteJobVacancy(Long id) {
         jobVacancyRepository.deleteById(id);
     }
 }
