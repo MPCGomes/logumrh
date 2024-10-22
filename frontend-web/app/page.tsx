adf48fa9fda67f2a30b03a792ae66754dc085aea
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-10-22 17:11:13 -0300

    feat: footer component

--- frontend-web/app/page.tsx
+++ frontend-web/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 };
