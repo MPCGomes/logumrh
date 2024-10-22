32c404a2bbf46d83a468e6e677dacbd14cd12b30
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-10-22 16:31:37 -0300

    refactor: remove unused code

--- frontend-web/app/page.tsx
+++ frontend-web/app/page.tsx
@@ -1,13 +1,9 @@
-import ButtonPrimary from "@/components/ButtonPrimary/ButtonPrimary";
-import ButtonSecondary from "@/components/ButtonSecondary/ButtonSecondary";
 import Navbar from "@/components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <ButtonPrimary text="teste"/>
-      <ButtonSecondary text="teste"/>
     </div>
   );
 };
