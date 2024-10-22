d5bddc4b142c1b33000999015934d91550b74eba
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-22 18:49:58 -0300

    add jwt response dto

---
+++ backend/src/main/java/com/logumrh/dto/auth/JwtResponseDTO.java
@@ -0,0 +1,13 @@
+package com.logumrh.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponseDTO {
+    private String token;
+    private String refreshToken;
+}
