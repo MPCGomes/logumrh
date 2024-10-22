96a30477688a19358f6feb2dc35a5c0deee0a8db
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-10-22 14:50:45 -0300

    styles: adding global styles

--- frontend-web/app/page.tsx
+++ frontend-web/app/page.tsx
@@ -1,8 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Simple Next.js 13+ Test</h1>
-      <p>This is just a basic text for testing purposes.</p>
+      <h1>Bom dia</h1>
     </div>
   );
 };
