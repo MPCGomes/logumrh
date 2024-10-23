fad5ee154706d6ef413210b2834ec56783fe78f1
Marcos Gomes <mpcgomes2@gmail.com>
2024-10-23 18:21:46 -0300

    add next config setup

--- frontend-web/next.config.ts
+++ frontend-web/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+  },
+  webpack(config) {
+    return config;
+  },
 };
 
 export default nextConfig;
