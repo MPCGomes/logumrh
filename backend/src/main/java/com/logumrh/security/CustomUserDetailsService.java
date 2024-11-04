348a517cd4ece064b411c7a914cea0f1f62046f5
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-03 22:46:46 -0300

    update custom user details

--- backend/src/main/java/com/logumrh/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/logumrh/security/CustomUserDetailsService.java
@@ -3,9 +3,7 @@ package com.logumrh.security;
 import com.logumrh.model.user.User;
 import com.logumrh.repository.user.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -24,10 +22,15 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
+        String roleName = user.getRole().getName();
+        if (!roleName.startsWith("ROLE_")) {
+            roleName = "ROLE_" + roleName;
+        }
+
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName()))
+                Collections.singletonList(new SimpleGrantedAuthority(roleName))
         );
     }
 }
