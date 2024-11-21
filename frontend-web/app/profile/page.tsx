ee5694043e0204520d924b016678ad40b4774d3e
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-21 20:33:39 -0300

    fix merge conflicts

--- frontend-web/app/profile/page.tsx
+++ frontend-web/app/profile/page.tsx
@@ -120,6 +120,10 @@ const profileData: Section[] = [
 const Profile: React.FC = () => (
   <div className="container">
     <ProfilePicture name={"Maria Júlia"} />
+    <ProfileNav
+      profileClass={'active'}
+      cvClass={'inactive'}
+    />
     <div className={styles.sections}>
       {profileData.map((section, index) => (
         <ProfileSection
@@ -128,63 +132,6 @@ const Profile: React.FC = () => (
           fields={section.fields}
         />
       ))}
-=======
-const Profile: React.FC = () => {
-  return (
-    <div className="container paddingSection">
-      <div className={styles.sections}>
-        <ProfileNav
-          profileClass={'active'}
-          cvClass={'inactive'}
-        />
-        <div className={styles.section}>
-          <h2>
-            Informações Básicas
-          </h2>
-          <div>
-            <ProfileField label="Nome" value="Maria Júlia" />
-            <ProfileField label="Nome Social" value="Julhão" />
-            <ProfileField label="Data de Nascimento" value="30 de setembro de 1999" />
-            <ProfileField label="CPF" value="000.000.000-00" />
-            <ProfileField label="Gênero" value="Fêmea" />
-          </div>
-        </div>
-        <div className={styles.section}>
-          <h2>
-            Informações Adicionais
-          </h2>
-          <div>
-            <ProfileField label="Etnia" value="Branca" />
-            <ProfileField label="Estado Civil" value="Solteirona" />
-            <ProfileField label="Deficiência" value="Nenhuma" />
-            <ProfileField label="Escolaridade" value="Superior" />
-          </div>
-        </div>
-        <div className={styles.section}>
-          <h2>
-            Contato & Segurança
-          </h2>
-          <div>
-            <ProfileField label="Telefone" value="(00) 0 0000-0000" />
-            <ProfileField label="E-mail" value="julhao@email.com" />
-            <ProfileField label="Senha" value="********" />
-          </div>
-        </div>
-        <div className={styles.section}>
-          <h2>
-            Endereço
-          </h2>
-          <div>
-            <ProfileField label="País" value="Brasil" />
-            <ProfileField label="CEP" value="00000-000" />
-            <ProfileField label="Estado" value="São Paulo" />
-            <ProfileField label="Cidade" value="Jacaré" />
-            <ProfileField label="Bairro" value="Formigas de Galocha" />
-            <ProfileField label="Rua/Avenida" value="Rua Flaminguitios" />
-            <ProfileField label="Complemento" value="Nenhum" />
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 );
