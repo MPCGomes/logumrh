9a82df0b2c62640fc45e88aea68be37a7cecb4c3
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-23 20:53:10 -0300

    update search bar styles

--- frontend-web/components/SearchBar/SearchBar.tsx
+++ frontend-web/components/SearchBar/SearchBar.tsx
@@ -3,14 +3,13 @@ import InputBase from '@mui/material/InputBase';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/material/styles';
-import styles from './SearchBar.module.scss';
 
 const StyledSearchInput = styled(InputBase)({
   width: '100%',
   borderRadius: 50,
-  outline: '1px solid #3a264040',
+  border: '1px solid #3a264040',
   backgroundColor: '#fff',
-  padding: '0px 24px',
+  padding: '0px 12px',
   height: '44px',
   '& .MuiInputBase-input': {
     color: 'var(--text-color)',
@@ -19,11 +18,10 @@ const StyledSearchInput = styled(InputBase)({
     },
   },
   '& .MuiSvgIcon-root': {
-    color: '#3a2640',
+    color: '#3a264040',
   },
   '&.Mui-focused': {
-    outline: '1px solid #3a2640',
-    borderColor: 'var(--primary-color)',
+    outline: '1px solid #3a264040',
   },
   '@media (min-width: 1024px)': {
     width: '80%'
@@ -34,11 +32,11 @@ interface SearchBarProps {
   placeholder?: string;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ placeholder = 'Search...' }) => (
+const SearchBar: React.FC<SearchBarProps> = ({ placeholder }) => (
   <StyledSearchInput
     endAdornment={
       <InputAdornment position="end">
-        <SearchIcon className={styles.icon} />
+        <SearchIcon />
       </InputAdornment>
     }
     placeholder={placeholder}
