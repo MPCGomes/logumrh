daaa3de28558dcf52e2aefe1b06df1c9d3b3c87d
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-24 23:08:34 -0300

    add custom theme provider for mui

--- frontend-web/app/layout.tsx
+++ frontend-web/app/layout.tsx
@@ -4,15 +4,10 @@ import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import BackToTop from "@/components/BackToTop/BackToTop";
 import WppFloating from "@/components/WppFloating/WppFloating";
+import CustomThemeProvider from "@/components/CustomThemeProvider/CustomThemeProvider";
 
 export const metadata = {
   title: "Logum RH",
-  link: [
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    },
-  ],
 };
 
 interface LayoutProps {
@@ -23,11 +18,13 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <BackToTop />
-        <WppFloating />
-        {children}
-        <Footer />
+        <CustomThemeProvider>
+          <Navbar />
+          <BackToTop />
+          <WppFloating />
+          {children}
+          <Footer />
+        </CustomThemeProvider>
       </body>
     </html>
   );
---
+++ frontend-web/components/CustomThemeProvider/CustomThemeProvider.tsx
@@ -0,0 +1,55 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { ReactNode } from "react";
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7e053a",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#7e053a",
+      contrastText: "#ffffff",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          backgroundColor: "#7e053a",
+          "&:hover": {
+            backgroundColor: "#5a032880",
+          },
+        },
+        outlined: {
+          borderColor: "#7e053a",
+          color: "#7e053a",
+          "&:hover": {
+            borderColor: "#7e053a",
+            backgroundColor: "#7e053a1a",
+          },
+        },
+        text: {
+          color: "#7e053a",
+          "&:hover": {
+            backgroundColor: "#7e053a1a",
+          },
+        },
+      },
+    },
+  },
+});
+
+const CustomThemeProvider = ({ children }: ThemeProps) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {children}
+  </ThemeProvider>
+);
+
+export default CustomThemeProvider;
