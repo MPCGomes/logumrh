f9cf67bb1badd0860669384ce81847e19119b4ba
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-24 23:58:46 -0300

    replaced cta button with mui

--- frontend-web/components/CTA/CTA.tsx
+++ frontend-web/components/CTA/CTA.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import styles from './CTA.module.scss'
+import CustomButton from '../CustomButton/CustomButton';
 
 const CTA = () => {
   return (
     <section className={styles.cta}>
       <div className='container'>
-        <p className={styles.text}>Toque no botão para falar consoco por WhatsApp e agende um atendimento agora mesmo!</p>
-        <button>
+        <p className={styles.text}>
+          Toque no botão para falar consoco por WhatsApp e agende um atendimento agora mesmo!
+        </p>
+        <CustomButton
+          variant="negativeOutlined"
+          color="primary"
+          onClick={() => {
+            document.querySelector("#contacts")?.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
           Fale Conosco
-        </button>
+        </CustomButton>
       </div>
     </section>
   )
