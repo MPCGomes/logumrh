ee620e4a97dae34ff99e353ec7374ec4b9518441
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-25 17:13:49 -0300

    update buttons on modal apply

--- frontend-web/components/ModalApply/ModalApply.tsx
+++ frontend-web/components/ModalApply/ModalApply.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from './ModalApply.module.scss'
-import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
+import { Button, Radio, RadioGroup, FormControlLabel, FormLabel } from "@mui/material";
+import styles from './ModalApply.module.scss';
 
 interface ModalApplyProps {
   isOpen: boolean;
   onClose: () => void;
+  onApply: () => void;
 }
 
-const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose }) => {
+const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose, onApply }) => {
   if (!isOpen) return null;
 
   return (
@@ -15,91 +16,80 @@ const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose }) => {
       <div className={styles.modal}>
         <p className={styles.title}>Nome da Vaga</p>
         <form className={styles.cvContainer}>
-          <label className={styles.radioCard}>
-            <input
-              type="radio"
-              name="option"
+          <FormLabel component="legend"></FormLabel>
+          <RadioGroup
+            name="cvOptions"
+            row
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexWrap: 'nowrap',
+            }}
+          >
+            <FormControlLabel
               value="option1"
+              control={<Radio />}
+              label={
+                <div className={styles.cardContent}>
+                  <h3>Nome do Currículo</h3>
+                  <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/resume-curriculum-vitae-design-template-cd5136dd58591765d75125a12226d1cd_screen.jpg?ts=1674136405" alt="" />
+                </div>
+              }
             />
-            <div className={styles.cardContent}>
-              <h3>Nome do Currículo</h3>
-              <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/resume-curriculum-vitae-design-template-cd5136dd58591765d75125a12226d1cd_screen.jpg?ts=1674136405" alt="" />
-            </div>
-          </label>
-          <label className={styles.radioCard}>
-            <input
-              type="radio"
-              name="option"
+            <FormControlLabel
               value="option2"
+              control={<Radio />}
+              label={
+                <div className={styles.cardContent}>
+                  <h3>Nome do Currículo</h3>
+                  <img src="https://cdn-images.livecareer.com.br/pages/word_lc_br_1.jpg" alt="" />
+                </div>
+              }
             />
-            <div className={styles.cardContent}>
-              <h3>Nome do Currículo</h3>
-              <img src="https://cdn-images.livecareer.com.br/pages/word_lc_br_1.jpg" alt="" />
-            </div>
-          </label>
-          <label className={styles.radioCard}>
-            <input
-              type="radio"
-              name="option"
+            <FormControlLabel
               value="option3"
+              control={<Radio />}
+              label={
+                <div className={styles.cardContent}>
+                  <h3>Nome do Currículo</h3>
+                  <img src="https://www.meucurriculoperfeito.com.br/wp-content/uploads/sites/6/2024/04/Curriculum-vitae-para-primeiro-emprego.svg" alt="" />
+                </div>
+              }
             />
-            <div className={styles.cardContent}>
-              <h3>Nome do Currículo</h3>
-              <img src="https://www.meucurriculoperfeito.com.br/wp-content/uploads/sites/6/2024/04/Curriculum-vitae-para-primeiro-emprego.svg" alt="" />
-            </div>
-          </label>
+          </RadioGroup>
         </form>
         <form className={styles.locationContainer}>
-          <h3>Você reside em Pindamonhangaba?</h3>
-          <input
-            type="radio"
-            name="imagem"
-            id="i1"
-          />
-          <label htmlFor="i1">
-            Sim
-          </label>
-          <input
-            type="radio"
-            name="imagem"
-            id="i2"
-          />
-          <label htmlFor="i2">
-            Não
-          </label>
+          <FormLabel component="legend">Você reside em Pindamonhangaba?</FormLabel>
+          <RadioGroup name="residencia">
+            <FormControlLabel value="sim" control={<Radio />} label="Sim" />
+            <FormControlLabel value="nao" control={<Radio />} label="Não" />
+          </RadioGroup>
         </form>
         <form className={styles.locationContainer}>
-          <h3>Você está matriculado em curso superior em administração, contabilidade ou áreas correlatadas?</h3>
-          <input
-            type="radio"
-            name="imagem"
-            id="j1"
-          />
-          <label htmlFor="j1">
-            Sim
-          </label>
-          <input
-            type="radio"
-            name="imagem"
-            id="j2"
-          />
-          <label htmlFor="j2">
-            Não
-          </label>
+          <FormLabel component="legend">Você está matriculado em curso superior em administração, contabilidade ou áreas correlatas?</FormLabel>
+          <RadioGroup name="matricula">
+            <FormControlLabel value="sim" control={<Radio />} label="Sim" />
+            <FormControlLabel value="nao" control={<Radio />} label="Não" />
+          </RadioGroup>
         </form>
         <div className={styles.buttonContainer}>
-          <p
+          <Button
+            variant="outlined"
+            color="primary"
             onClick={onClose}
-            className={styles.closeButton}
           >
             Fechar
-          </p>
-          <ButtonPrimary
-            text={"Enviar"}
-          />
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+          >
+            Candidatar-se
+          </Button>
+
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
