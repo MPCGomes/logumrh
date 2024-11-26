94f9d7181b49b02cdd17c6a4d8c03d3fa8bc3aee
Marcos Gomes <mpcgomes2@gmail.com>
2024-11-25 22:33:07 -0300

    modal apply fix

--- frontend-web/components/ModalApply/ModalApply.tsx
+++ frontend-web/components/ModalApply/ModalApply.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Button, Radio, RadioGroup, FormControlLabel, FormLabel } from "@mui/material";
-import styles from './ModalApply.module.scss';
+import styles from './ModalApply.module.scss'
+import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
+import { Button } from "@mui/material";
 
 interface ModalApplyProps {
   isOpen: boolean;
   onClose: () => void;
-  onApply: () => void;
 }
 
-const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose, onApply }) => {
+const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
@@ -16,61 +16,78 @@ const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose, onApply }) => 
       <div className={styles.modal}>
         <p className={styles.title}>Selecione seu currículo</p>
         <form className={styles.cvContainer}>
-          <FormLabel component="legend"></FormLabel>
-          <RadioGroup
-            name="cvOptions"
-            row
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              flexWrap: 'nowrap',
-            }}
-          >
-            <FormControlLabel
+          <label className={styles.radioCard}>
+            <input
+              type="radio"
+              name="option"
               value="option1"
-              control={<Radio />}
-              label={
-                <div className={styles.cardContent}>
-                  <h3>Informática</h3>
-                  <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/resume-curriculum-vitae-design-template-cd5136dd58591765d75125a12226d1cd_screen.jpg?ts=1674136405" alt="" />
-                </div>
-              }
             />
-            <FormControlLabel
+            <div className={styles.cardContent}>
+              <h3>Atendimento</h3>
+              <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/resume-curriculum-vitae-design-template-cd5136dd58591765d75125a12226d1cd_screen.jpg?ts=1674136405" alt="" />
+            </div>
+          </label>
+          <label className={styles.radioCard}>
+            <input
+              type="radio"
+              name="option"
               value="option2"
-              control={<Radio />}
-              label={
-                <div className={styles.cardContent}>
-                  <h3>Administrativo</h3>
-                  <img src="https://cdn-images.livecareer.com.br/pages/word_lc_br_1.jpg" alt="" />
-                </div>
-              }
             />
-            <FormControlLabel
+            <div className={styles.cardContent}>
+              <h3>Administrativo</h3>
+              <img src="https://cdn-images.livecareer.com.br/pages/word_lc_br_1.jpg" alt="" />
+            </div>
+          </label>
+          <label className={styles.radioCard}>
+            <input
+              type="radio"
+              name="option"
               value="option3"
-              control={<Radio />}
-              label={
-                <div className={styles.cardContent}>
-                  <h3>Contábil</h3>
-                  <img src="https://www.meucurriculoperfeito.com.br/wp-content/uploads/sites/6/2024/04/Curriculum-vitae-para-primeiro-emprego.svg" alt="" />
-                </div>
-              }
             />
-          </RadioGroup>
+            <div className={styles.cardContent}>
+              <h3>Informática</h3>
+              <img src="https://www.meucurriculoperfeito.com.br/wp-content/uploads/sites/6/2024/04/Curriculum-vitae-para-primeiro-emprego.svg" alt="" />
+            </div>
+          </label>
         </form>
         <form className={styles.locationContainer}>
-          <FormLabel component="legend">Você reside em Pindamonhangaba?</FormLabel>
-          <RadioGroup name="residencia">
-            <FormControlLabel value="sim" control={<Radio />} label="Sim" />
-            <FormControlLabel value="nao" control={<Radio />} label="Não" />
-          </RadioGroup>
+          <h3>Você reside em Pindamonhangaba?</h3>
+          <input
+            type="radio"
+            name="imagem"
+            id="i1"
+          />
+          <label htmlFor="i1">
+            Sim
+          </label>
+          <input
+            type="radio"
+            name="imagem"
+            id="i2"
+          />
+          <label htmlFor="i2">
+            Não
+          </label>
         </form>
         <form className={styles.locationContainer}>
-          <FormLabel component="legend">Você está matriculado em curso superior em administração, contabilidade ou áreas correlatas?</FormLabel>
-          <RadioGroup name="matricula">
-            <FormControlLabel value="sim" control={<Radio />} label="Sim" />
-            <FormControlLabel value="nao" control={<Radio />} label="Não" />
-          </RadioGroup>
+          <h3>Você está matriculado em curso superior em administração, <br></br>
+            contabilidade ou áreas correlatadas?</h3>
+          <input
+            type="radio"
+            name="imagem"
+            id="j1"
+          />
+          <label htmlFor="j1">
+            Sim
+          </label>
+          <input
+            type="radio"
+            name="imagem"
+            id="j2"
+          />
+          <label htmlFor="j2">
+            Não
+          </label>
         </form>
         <div className={styles.buttonContainer}>
           <Button
@@ -89,7 +106,7 @@ const ModalApply: React.FC<ModalApplyProps> = ({ isOpen, onClose, onApply }) => 
 
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
