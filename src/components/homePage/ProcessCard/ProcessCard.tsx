2800c20b44ac0ffd03db22fa7bd21de6f7772b56
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-17 14:50:03 -0300

    feat: process card component

---
+++ src/components/homePage/ProcessCard/ProcessCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./ProcessCard.module.scss";
+
+interface ProcessCardProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+const ProcessCard: React.FC<ProcessCardProps> = ({
+  icon,
+  title,
+  description,
+}) => {
+  return (
+    <div className={styles.processCard}>
+      <img src={icon} alt="error" />
+      <div className={styles.text}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProcessCard;
