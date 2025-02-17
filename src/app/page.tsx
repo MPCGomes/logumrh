f5de410b18d3a92affad71778138a3d4175f6f52
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-17 14:11:55 -0300

    feat: cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+
+import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return <div className={styles.page}>
+    <CtaSection />
   </div>;
 }
