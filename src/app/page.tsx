5d0fefe2c8249ad0010343e1770d501fa506c7e7
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-17 15:07:29 -0300

    feat: process section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
-import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
 import styles from "./page.module.scss";
+import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
+import ProcessSection from "@/containers/homePage/ProcessSection/ProcessSection";
 
 export default function Home() {
   return <div className={styles.page}>
+    <ProcessSection />
     <CtaSection />
   </div>;
 }
