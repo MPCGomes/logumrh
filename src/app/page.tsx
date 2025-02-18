84a500e4c94e5d9c5cffdbd57405e499c3bf9d0a
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-18 12:01:03 -0300

    add faq section on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import styles from "./page.module.scss";
 import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
+import FaqSection from "@/containers/homePage/FaqSection/FaqSection";
 import ProcessSection from "@/containers/homePage/ProcessSection/ProcessSection";
 
 export default function Home() {
-  return <div className={styles.page}>
-    <ProcessSection />
-    <CtaSection />
-  </div>;
+  return (
+    <div className={styles.page}>
+      <ProcessSection />
+      <FaqSection />
+      <CtaSection />
+    </div>
+  );
 }
