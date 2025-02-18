7a3ab8cf0ee65325367d04300411e305cee8bc1a
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-18 12:12:52 -0300

    feat: testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
 import FeaturesSection from "@/containers/homePage/FeaturesSection/FeaturesSection";
 import HeroSection from "@/containers/homePage/HeroSection/HeroSection";
 import ProcessSection from "@/containers/homePage/ProcessSection/ProcessSection";
+import TestimonialsSection from "@/containers/homePage/TestimonialsSection/TestimonialsSection";
 
 export default function Home() {
   return <div className={styles.page}>
     <HeroSection />
     <ProcessSection />
     <FeaturesSection />
+    <TestimonialsSection />
     <CtaSection />
   </div>;
 }
---
+++ src/containers/homePage/TestimonialsSection/TestimonialsSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./TestimonialsSection.module.scss";
+import clsx from "clsx";
+import SectionHeading from "@/components/common/SectionHeading/SectionHeading";
+import Testimonial from "@/components/homePage/Testimonial/Testimonial";
+
+const TestimonialsSection = () => {
+  return (
+    <section className={clsx(styles.heroContainer, "container section")}>
+      <SectionHeading
+        variant="center"
+        subheading="Confira os nossos"
+        heading="Depoimentos"
+      />
+      <div className={styles.gridContainer}>
+        <Testimonial
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend ligula ut leo cursus, ut interdum nisl lobortis."
+          }
+          image={
+            "https://s2.glbimg.com/KpzbDutodg_CHzXGzvvN79974fw=/e.glbimg.com/og/ed/f/original/2017/01/02/benedict-cumberbatch_destaque.jpg"
+          }
+          name={"SHERLOCK HOLMES"}
+          role={"Lorem ipsum dolor"}
+        />
+        <Testimonial
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend ligula ut leo cursus, ut interdum nisl lobortis."
+          }
+          image={
+            "https://s2.glbimg.com/KpzbDutodg_CHzXGzvvN79974fw=/e.glbimg.com/og/ed/f/original/2017/01/02/benedict-cumberbatch_destaque.jpg"
+          }
+          name={"SHERLOCK HOLMES"}
+          role={"Lorem ipsum dolor"}
+        />
+        <Testimonial
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend ligula ut leo cursus, ut interdum nisl lobortis."
+          }
+          image={
+            "https://s2.glbimg.com/KpzbDutodg_CHzXGzvvN79974fw=/e.glbimg.com/og/ed/f/original/2017/01/02/benedict-cumberbatch_destaque.jpg"
+          }
+          name={"SHERLOCK HOLMES"}
+          role={"Lorem ipsum dolor"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialsSection;
