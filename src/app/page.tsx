b26bdf89000b83a92d4cdbf63475cde58acdc874
Marcos Gomes <mpcgomes2@gmail.com>
2025-02-18 14:33:40 -0300

    add job section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import CtaSection from "@/containers/homePage/CtaSection/CtaSection";
 import FeaturesSection from "@/containers/homePage/FeaturesSection/FeaturesSection";
 import HeroSection from "@/containers/homePage/HeroSection/HeroSection";
 import ProcessSection from "@/containers/homePage/ProcessSection/ProcessSection";
+import JobSection from "@/containers/homePage/JobsSection/JobsSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <HeroSection />
       <ProcessSection />
+      <JobSection />
       <FeaturesSection />
       <FaqSection />
       <CtaSection />
