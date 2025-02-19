0f146b79fb7008f140ef9453bf9555cdb4eba490
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-19 14:52:37 -0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // ✅ Enables static export (Replaces `next export`)
+  output: 'export',
+  images: {
+    unoptimized: true, 
+  },
 };
 
 module.exports = nextConfig;
