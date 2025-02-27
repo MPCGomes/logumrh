744d37542de58b5c4625624e7f2fa1f2e63abdb3
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-02-27 20:50:48 -0300

    styles: paragraph color update

--- src/components/homePage/FeatureCard/FeatureCard.tsx
+++ src/components/homePage/FeatureCard/FeatureCard.tsx
@@ -10,11 +10,11 @@ interface FeatureCardProps {
 const FeatureCard: React.FC<FeatureCardProps> = ({ title, description }) => {
   return (
     <div className={styles.featureCard}>
-      <p>
+      <p className={styles.icon}>
         <PeopleOutlineIcon />
       </p>
-      <p>{title}</p>
-      <p>{description}</p>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.description}>{description}</p>
     </div>
   );
 };
