912841c8292799042e99ad84d72176cc5d4873ab
Marcos Gomes <mpcgomes2@gmail.com>
2025-03-08 18:54:32 -0300

    refactor hero section

--- src/containers/homePage/HeroSection/HeroSection.tsx
+++ src/containers/homePage/HeroSection/HeroSection.tsx
@@ -1,26 +1,23 @@
-import Button from "@/components/common/Button/Button";
-import React from "react";
+import { FC } from "react";
 import styles from "./HeroSection.module.scss";
 import clsx from "clsx";
+import Button from "@/components/common/Button/Button";
 
-const HeroSection = () => {
-  return (
-    <section className={clsx(styles.heroContainer, "container section")}>
-      <div className={styles.textContainer}>
-        <h1>Processos seletivos humanizados e eficientes</h1>
-        <h3>
-          A melhor solução para seu Recrutamento e Seleção! Economize tempo e
-          dinheiro e encontre o candidato ideal com a nossa equipe
-          especializada.
-        </h3>
-        <div className={styles.buttonContainer}>
-          <Button variant="contained">Quero Contratar</Button>
-          <Button>Ver Vagas</Button>
-        </div>
+const HeroSection: FC = () => (
+  <section className={clsx(styles.heroContainer, "container section")}>
+    <div className={styles.textContainer}>
+      <h1>Processos seletivos humanizados e eficientes</h1>
+      <h3>
+        A melhor solução para seu Recrutamento e Seleção! Economize tempo e
+        dinheiro e encontre o candidato ideal com nossa equipe especializada.
+      </h3>
+      <div className={styles.buttonContainer}>
+        <Button variant="contained">Quero Contratar</Button>
+        <Button>Ver Vagas</Button>
       </div>
-      <img src="/assets/hero-img.svg" alt="hero-img" />
-    </section>
-  );
-};
+    </div>
+    <img src="/assets/hero-img.svg" alt="Processo de recrutamento" />
+  </section>
+);
 
 export default HeroSection;
