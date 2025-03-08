ce7a4fc238b7e892681e82ecf573e76cf4997966
Marcos Gomes <mpcgomes2@gmail.com>
2025-03-08 18:55:48 -0300

    refactor service card

--- src/components/homePage/ServiceCard/ServiceCard.tsx
+++ src/components/homePage/ServiceCard/ServiceCard.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import { FC, ReactNode } from "react";
 import styles from "./ServiceCard.module.scss";
 import CheckIcon from "@mui/icons-material/Check";
-import { ReactNode } from "react";
 
 interface ServiceCardProps {
   src: string;
@@ -11,37 +10,31 @@ interface ServiceCardProps {
   icon: ReactNode;
 }
 
-const ServiceCard: React.FC<ServiceCardProps> = ({
+const ServiceCard: FC<ServiceCardProps> = ({
   src,
   title,
   description,
   features,
   icon,
-}) => {
-  return (
-    <div className={styles.serviceCard}>
-      <div className={styles.image}>
-        <img src={src} alt="process-image" />
-      </div>
-      <div className={styles.cardInfo}>
-        <div className={styles.icon}>
-          <p>{icon}</p>
-        </div>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.description}>{description}</p>
-        <ul className={styles.features}>
-          {features.map((feature, index) => (
-            <li key={index}>
-              <span>
-                <CheckIcon sx={{ fontSize: 20 }} />
-              </span>
-              {feature}
-            </li>
-          ))}
-        </ul>
-      </div>
+}) => (
+  <div className={styles.serviceCard}>
+    <div className={styles.image}>
+      <img src={src} alt="service-image" />
     </div>
-  );
-};
+    <div className={styles.cardInfo}>
+      <div className={styles.icon}>{icon}</div>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.features}>
+        {features.map((feature, index) => (
+          <li key={index}>
+            <CheckIcon className={styles.checkIcon} />
+            {feature}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
 
 export default ServiceCard;
