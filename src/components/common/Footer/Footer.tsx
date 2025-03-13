949c2870c90acfaeacd728d36f63d125b937668a
MPCGomes <mpcgomes2@gmail.com>
2025-03-13 18:50:20 -0300

    refactor footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,83 +1,95 @@
+import { FC } from "react";
 import Link from "next/link";
-import React from "react";
 import styles from "./Footer.module.scss";
 
-const Footer = () => {
+interface FooterLink {
+  name: string;
+  href?: string;
+  external?: boolean;
+}
+
+interface FooterSection {
+  title: string;
+  links: FooterLink[];
+}
+
+const footerLinks: FooterSection[] = [
+  {
+    title: "Links",
+    links: [
+      { name: "Início", href: "/" },
+      { name: "Vagas", href: "/jobs" },
+    ],
+  },
+  {
+    title: "Sobre Nós",
+    links: [
+      { name: "Caraguatatuba, SP" },
+      { name: "contato@logumrh.com" },
+      { name: "+55 (12) 97409-6393" },
+    ],
+  },
+  {
+    title: "Redes Sociais",
+    links: [
+      {
+        name: "@logumrh",
+        href: "https://www.instagram.com/logumrh/",
+        external: true,
+      },
+      {
+        name: "linkedin.com/LogumRH",
+        href: "https://www.linkedin.com/company/logumrh/posts/?feedView=all",
+        external: true,
+      },
+    ],
+  },
+];
+
+const Footer: FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.infoContainer}>
+        <div className={styles.info}>
           <img
             className={styles.logo}
-            src="./assets/logo_horizontal.png"
-            alt="logum-recursos-humanos-logo"
+            src="/assets/logo_horizontal.png"
+            alt="Logum RH Logo"
           />
 
-          <div className={styles.groupContainer}>
-            <p className={styles.title}>Links</p>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <Link className={styles.link} href={"/"}>
-                  Início
-                </Link>
-              </li>
-              <li className={styles.item}>
-                <Link className={styles.link} href={"/jobs"}>
-                  Vagas
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className={styles.groupContainer}>
-            <p className={styles.title}>Sobre Nós</p>
-            <ul className={styles.list}>
-              <li className={styles.item}>Caraguatatuba, SP</li>
-              <li className={styles.item}>contato@logumrh.com</li>
-              <li className={styles.item}>+55 (12) 97409-6393</li>
-            </ul>
-          </div>
-
-          <div className={styles.groupContainer}>
-            <p className={styles.title}>Redes Sociais</p>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <Link
-                  className={styles.link}
-                  href={"https://www.instagram.com/logumrh/"}
-                  target="_blank"
-                >
-                  @logumrh
-                </Link>
-              </li>
-              <li className={styles.item}>
-                <Link
-                  className={styles.link}
-                  href={
-                    "https://www.linkedin.com/company/logumrh/posts/?feedView=all"
-                  }
-                  target="_blank"
-                >
-                  linkedin.com/LogumRH
-                </Link>
-              </li>
-            </ul>
-          </div>
+          {footerLinks.map(({ title, links }, index) => (
+            <div key={index} className={styles.group}>
+              <p className={styles.title}>{title}</p>
+              <ul>
+                {links.map((link, linkIndex) => (
+                  <li key={linkIndex}>
+                    {link.href ? (
+                      <Link
+                        href={link.href}
+                        target={link.external ? "_blank" : "_self"}
+                      >
+                        {link.name}
+                      </Link>
+                    ) : (
+                      link.name
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
 
         <hr className={styles.hr} />
 
-        <div className={styles.copyrightContainer}>
+        <div className={styles.copyright}>
           <p>© 2025 Todos os direitos reservados.</p>
           <p>
-            Desenvolvido por{" "}
-            <Link
-              className={styles.link}
-              href={"https://niloware.com.br/"}
-              target="_blank"
-            >
+            Desenvolvido por {""}
+            <Link href="https://niloware.com.br/" target="_blank">
               <strong>Niloware</strong>
             </Link>
+            .
           </p>
         </div>
       </div>
