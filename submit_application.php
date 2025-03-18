03c5bdaae5124efab971313ba89af98258c2c6c9
MPCGomes <mpcgomes2@gmail.com>
2025-03-18 17:38:38 -0300

    fix submit application

--- submit_application.php
+++ submit_application.php
@@ -1,5 +1,8 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
