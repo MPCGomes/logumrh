d6ae2cdc2e5b72edff4591517574b691352da09f
MPCGomes <mpcgomes2@gmail.com>
2025-03-18 17:39:10 -0300

    fix slug pages loading

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async exportPathMap() {
+    const jobsData: { slug: string }[] = require("./src/data/jobs.json");
+
+    const paths: Record<string, { page: string; query?: { slug: string } }> = {
+      "/": { page: "/" },
+    };
+
+    jobsData.forEach((job: { slug: string }) => {
+      paths[`/jobs/${job.slug}`] = {
+        page: "/jobs/[slug]",
+        query: { slug: job.slug },
+      };
+    });
+
+    return paths;
+  },
 };
 
 module.exports = nextConfig;
