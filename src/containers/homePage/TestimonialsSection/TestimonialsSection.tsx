aaf5d28df7ad6016703f3316438fcff557f9c66f
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-03-19 18:08:16 -0300

    update testimonials

--- src/containers/homePage/TestimonialsSection/TestimonialsSection.tsx
+++ src/containers/homePage/TestimonialsSection/TestimonialsSection.tsx
@@ -8,16 +8,16 @@ const defaultProfileImage = "/assets/profile-placeholder.jpg";
 
 const testimonials = [
   {
-    text: "A Logum foi essencial para encontrar estagiários qualificados para a Menconi Contabilidade. Tive dificuldades em acessar currículos por meio das universidades, mas a Logum cuidou de todo o processo, desde o contato com as instituições até a seleção dos candidatos. Recebi quatro perfis muito qualificados e consegui contratar o estagiário ideal. Excelente suporte e eficiência!",
-    image: defaultProfileImage,
-    name: "Olivia Menconi",
-    role: "Proprietária Menconi Contabilidade",
-  },
-  {
     text: "A Logum nos ajudou a encontrar candidatos altamente qualificados para nossas franquias. O processo de recrutamento foi ágil e eficiente, permitindo-nos focar no crescimento da Caraguá Shake sem preocupações com contratações.",
     image: defaultProfileImage,
     name: "Adriana Scotti",
     role: "Proprietária Caraguá Shake",
+  },
+  {
+    text: "A Logum foi essencial para encontrar estagiários qualificados para a Menconi Contabilidade. Tive dificuldades em acessar currículos por meio das universidades, mas a Logum cuidou de todo o processo, desde o contato com as instituições até a seleção dos candidatos. Recebi quatro perfis muito qualificados e consegui contratar o estagiário ideal. Excelente suporte e eficiência!",
+    image: defaultProfileImage,
+    name: "Olivia Menconi",
+    role: "Proprietária Menconi Contabilidade",
   },
   {
     text: "Com a Logum, conseguimos recrutar desenvolvedores com precisão e rapidez. A triagem e os testes técnicos foram conduzidos com excelência, garantindo contratações certeiras para nossa equipe.",
@@ -28,16 +28,20 @@ const testimonials = [
 ];
 
 const TestimonialsSection: FC = () => (
-  <section className={clsx(styles.testimonialsContainer, "container section")}>
-    <SectionHeading
-      variant="center"
-      subheading="O que dizem sobre nós"
-      heading="Depoimentos de nossos clientes"
-    />
-    <div className={styles.gridContainer}>
-      {testimonials.map((testimonial, index) => (
-        <Testimonial key={index} {...testimonial} />
-      ))}
+  <section className="primaryBackground">
+    <div className={clsx(styles.testimonialsContainer, "container section")}>
+      <SectionHeading
+        headingColor="lightHeading"
+        subHeadingColor="lightSubHeading"
+        variant="center"
+        subheading="O que dizem sobre nós"
+        heading="Depoimentos de nossos clientes"
+      />
+      <div className={styles.gridContainer}>
+        {testimonials.map((testimonial, index) => (
+          <Testimonial key={index} {...testimonial} />
+        ))}
+      </div>
     </div>
   </section>
 );
