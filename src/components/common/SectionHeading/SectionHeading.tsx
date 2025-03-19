8b5de020f8116f76d6f753cb9e3176e682c29090
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-03-19 18:13:06 -0300

    update section heading

--- src/components/common/SectionHeading/SectionHeading.tsx
+++ src/components/common/SectionHeading/SectionHeading.tsx
@@ -4,18 +4,22 @@ import styles from "./SectionHeading.module.scss";
 
 interface SectionHeadingProps {
   variant?: "center" | "start";
+  headingColor?: "darkHeading" | "lightHeading";
+  subHeadingColor?: "darkSubHeading" | "lightSubHeading";
   subheading?: string;
   heading?: string;
 }
 
 const SectionHeading: FC<SectionHeadingProps> = ({
   variant = "center",
+  headingColor= "darkHeading",
+  subHeadingColor= "darkSubHeading",
   subheading = "",
   heading = "",
 }) => (
   <div className={clsx(styles.sectionHeading, styles[variant])}>
-    <p className={styles.subheading}>{subheading}</p>
-    <h2 className={styles.heading}>{heading}</h2>
+    <p className={clsx(styles.subheading, styles[subHeadingColor])}>{subheading}</p>
+    <h2 className={clsx(styles.heading, styles[headingColor])}>{heading}</h2>
   </div>
 );
 
