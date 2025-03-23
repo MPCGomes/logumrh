55b8bf910acf75b21da3d80fc2eb8be50f477c9a
MPCGomes <mpcgomes2@gmail.com>
2025-03-23 01:00:35 -0300

    add react slick to testimonials

---
+++ src/containers/homePage/TestimonialsCarousel/TestimonialsCarousel.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React from "react";
+import Slider from "react-slick";
+import Image from "next/image";
+
+interface Testimonial {
+  photo: string;
+  name: string;
+  title: string;
+  rating: number;
+  testimonial: string;
+}
+
+interface TestimonialsCarouselProps {
+  testimonials: Testimonial[];
+}
+
+const TestimonialsCarousel: React.FC<TestimonialsCarouselProps> = ({
+  testimonials,
+}) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings}>
+      {testimonials.map((testimonial, index) => (
+        <div key={index} className="testimonial">
+          <div className="testimonialText">
+            <p className="text">{testimonial.testimonial}</p>
+          </div>
+          <hr />
+          <div className="user">
+            <Image
+              className="image"
+              src={testimonial.photo}
+              alt={testimonial.name}
+              width={60}
+              height={60}
+            />
+            <div className="userInfo">
+              <h3 className="name">{testimonial.name}</h3>
+              <p className="role">{testimonial.title}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default TestimonialsCarousel;
--- src/containers/homePage/TestimonialsSection/TestimonialsSection.tsx
+++ src/containers/homePage/TestimonialsSection/TestimonialsSection.tsx
@@ -1,49 +1,80 @@
-import { FC } from "react";
-import styles from "./TestimonialsSection.module.scss";
-import clsx from "clsx";
-import SectionHeading from "@/components/common/SectionHeading/SectionHeading";
-import Testimonial from "@/components/homePage/Testimonial/Testimonial";
+"use client";
 
-const defaultProfileImage = "/assets/profile-placeholder.jpg";
+import { FC } from "react";
+import Slider from "react-slick";
+import Testimonial from "@/components/homePage/Testimonial/Testimonial";
+import SectionHeading from "@/components/common/SectionHeading/SectionHeading";
+import clsx from "clsx";
+import styles from "./TestimonialsSection.module.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const testimonials = [
   {
     text: "A Logum nos ajudou a encontrar candidatos altamente qualificados para nossas franquias. O processo de recrutamento foi ágil e eficiente, permitindo-nos focar no crescimento da Caraguá Shake sem preocupações com contratações.",
-    image: defaultProfileImage,
+    image: "/testimonial-avatars/caragua-shake.jpeg",
     name: "Adriana Scotti",
     role: "Proprietária Caraguá Shake",
   },
   {
-    text: "A Logum foi essencial para encontrar estagiários qualificados para a Menconi Contabilidade. Tive dificuldades em acessar currículos por meio das universidades, mas a Logum cuidou de todo o processo, desde o contato com as instituições até a seleção dos candidatos. Recebi quatro perfis muito qualificados e consegui contratar o estagiário ideal. Excelente suporte e eficiência!",
-    image: defaultProfileImage,
+    text: "A Logum foi essencial para encontrar estagiários qualificados para a Menconi Contabilidade. Tive dificuldades em acessar currículos por meio das universidades, mas a Logum cuidou de todo o processo, desde o contato com as instituições até a seleção dos candidatos. Excelente suporte e eficiência!",
+    image: "/testimonial-avatars/menconi.jpeg",
     name: "Olivia Menconi",
     role: "Proprietária Menconi Contabilidade",
   },
   {
     text: "Com a Logum, conseguimos recrutar desenvolvedores com precisão e rapidez. A triagem e os testes técnicos foram conduzidos com excelência, garantindo contratações certeiras para nossa equipe.",
-    image: defaultProfileImage,
-    name: "Marcos Paulo",
-    role: "Proprietário Niloware",
+    image: "/testimonial-avatars/niloware.jpeg",
+    name: "Júlia Miosso",
+    role: "Co-Proprietária Niloware",
   },
 ];
 
-const TestimonialsSection: FC = () => (
-  <section className="primaryBackground">
-    <div className={clsx(styles.testimonialsContainer, "container section")}>
-      <SectionHeading
-        headingColor="lightHeading"
-        subHeadingColor="lightSubHeading"
-        variant="center"
-        subheading="O que dizem sobre nós"
-        heading="Depoimentos de nossos clientes"
-      />
-      <div className={styles.gridContainer}>
-        {testimonials.map((testimonial, index) => (
-          <Testimonial key={index} {...testimonial} />
-        ))}
+const TestimonialsSection: FC = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <section className="primaryBackground">
+      <div className={clsx(styles.testimonialsContainer, "container section")}>
+        <SectionHeading
+          headingColor="lightHeading"
+          subHeadingColor="lightSubHeading"
+          variant="center"
+          subheading="O que dizem sobre nós"
+          heading="Depoimentos"
+        />
+        <Slider {...settings} className={styles.gridContainer}>
+          {testimonials.map((testimonial, index) => (
+            <div key={index} className={styles.testimonialWrapper}>
+              <Testimonial {...testimonial} />
+            </div>
+          ))}
+        </Slider>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default TestimonialsSection;
