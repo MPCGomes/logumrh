05ff2fe83f3124f6a7bc8344364e60d03d19d97a
Gikacete <logumrh@gmail.com>
2025-03-26 22:07:35 -0300

    update carousel speed
    faster

--- src/components/homePage/ClientCarousel/ClientCarousel.tsx
+++ src/components/homePage/ClientCarousel/ClientCarousel.tsx
@@ -23,7 +23,7 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 3000,
   cssEase: "linear",
   fade: false,
   centerMode: true,
